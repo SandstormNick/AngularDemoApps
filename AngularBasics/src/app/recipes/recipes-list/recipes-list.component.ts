763a9920e6ba59bc5734401f3c94cb1acbdf2d5f
SandstormNick <gibbensnick@gmail.com>
2023-09-05 16:37:16 +0200

    Adding content to the recipe-list component

--- AngularBasics/src/app/recipes/recipes-list/recipes-list.component.ts
+++ AngularBasics/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a test', 'https://bakesbybrownsugar.com/wp-content/uploads/2022/01/Chocolate-Rugelach-2.jpg.webp'),
+    new Recipe('Test Recipe 2', 'This is another test', 'https://bakesbybrownsugar.com/wp-content/uploads/2022/01/Chocolate-Rugelach-2.jpg.webp')
+  ];
 
 }
